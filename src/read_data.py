9a5911e262a07edbf3848187d780c8e170430aec
Sol Stappard <Solstappard01@gmail.com>
2025-12-11 09:49:10 +0700

    Pushing clean and read data files - can now read in the raw data. next step build out function in clean_data.py to remove all the data I dont want

--- src/read_data.py
+++ src/read_data.py
@@ -1,1 +1,43 @@
-from config import 
+from pathlib import Path
+import pandas as pd 
+
+from .config import load_config
+
+
+
+def _read_gem_file(path: Path) -> pd.DataFrame:
+    """Read GEM file into a pandas DataFrame."""
+
+    suffix = path.suffix
+
+    if suffix in [".xlsx", ".xls"]:
+        return pd.read_excel(path)
+
+    elif suffix in [".csv"]:
+        return pd.read_csv(path)
+    else:
+        raise ValueError(f"Unsupported file format for GEM fiel: {suffix}")
+
+
+def load_raw_gem_data() -> pd.DataFrame:
+    """Load raw GEM data from file."""
+
+    cfg = load_config()
+    path = cfg["paths"]["gem_file"]
+
+    if not path.exists():
+        raise FileNotFoundError(f"GEM file not found at: {path}")
+
+
+    df = _read_gem_file(path)
+    breakpoint()
+    return df
+
+
+if __name__ == "__main__":
+    df = load_raw_gem_data()
+
+
+
+
+
